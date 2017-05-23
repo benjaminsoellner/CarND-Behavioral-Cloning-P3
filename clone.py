272319b017f62e6a50215b5c4e9f253de54d1b7c
Benjamin Soellner <benjamin.soellner@gmail.com>
2017-05-23 23:24:38 +0000

    * image manipulation ** convert from BGR to RGB (imgOut = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) ** resize input by halving width & height * try batch size of 32 * change correction angle to -0.10 (right) and +0.10 (left) * only try either flipped vs. non-flipped image for every data point (np.random.uniform()>0.5) * add additional RGB -> single-channel convolutional layer to model * standardize to NVidia model * make sure no activation is pfrom keras iresent in last fully connected layer
    removed model_0.h5 cause too big

--- clone.py
+++ clone.py
@@ -3,17 +3,15 @@ import cv2
 import numpy as np
 import sklearn
 import sklearn.utils
-import sys
 import pandas as pd
-import os.path
+from keras import backend as K
 
 H5_FILE = "model_{0}.h5"
 CSV_FILE = "model_{0}.csv"
-MODEL = (sys.argv[1] if len(sys.argv)>0 else "simple")
-AUG_FACTOR = 6
+AUG_FACTOR = 3
 
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=128):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
@@ -22,21 +20,24 @@ def generator(samples, batch_size=32):
             images = []
             measurements = []
             for batch_sample in batch_samples:
-                for source_path_column, measurement_correction in zip([0, 1, 2], [1.0, -0.2, 0.2]):
+                for source_path_column, measurement_correction in zip([0, 1, 2], [0.0, -0.1, 0.1]):
                     source_path = batch_sample[source_path_column]
                     filename = source_path.split("/")[-1]
                     current_path = 'data/IMG/' + filename
                     image = cv2.imread(current_path)
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    image = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
                     images.append(image)
                     measurement = float(batch_sample[3])
                     measurements.append(measurement + measurement_correction)
             augmented_images, augmented_measurements = [], []
             for image, measurement in zip(images, measurements):
-                augmented_images.append(image)
-                augmented_measurements.append(measurement)
-                augmented_images.append(cv2.flip(image, 1))
-                augmented_measurements.append(measurement*-1.0)
-            # trim image to only see section with road
+                if np.random.uniform()>0.5:
+                    augmented_images.append(image)
+                    augmented_measurements.append(measurement)
+                else:
+                    augmented_images.append(cv2.flip(image, 1))
+                    augmented_measurements.append(measurement*-1.0)
             X_train = np.array(augmented_images)
             y_train = np.array(augmented_measurements)
             yield sklearn.utils.shuffle(X_train, y_train)
@@ -60,100 +61,48 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
 from keras.layers.convolutional import Convolution2D
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, MaxPooling2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, ELU
 
 model = Sequential()
-model.add(Lambda(lambda x: x/255.0-0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Lambda(lambda x: x/255.0-0.5, input_shape=(80,160,3)))
+model.add(Cropping2D(cropping=((35,12), (0,0))))
 
-history = None
+# let model pick best channel from linear combination RGB
+model.add(Convolution2D(1,1,1,activation="relu"))
+# NVidia pipeline
+model.add(Convolution2D(24,5,5,activation="relu"))
+model.add(Convolution2D(36,5,5,activation="relu"))
+model.add(Convolution2D(48,5,5,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer='adam')
+history = model.fit_generator(train_generator, validation_data=validation_generator,
+                    samples_per_epoch=len(train_samples)*AUG_FACTOR, nb_val_samples=len(validation_samples)*AUG_FACTOR,
+                    nb_epoch=5)
 
-if MODEL == "nvidia":
-    model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-    model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-    model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
-    model.add(Convolution2D(64,3,3,activation="relu"))
-    model.add(Convolution2D(64,3,3,activation="relu"))
-    model.add(Flatten())
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dense(10))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    history = model.fit_generator(train_generator, validation_data=validation_generator,
-                        samples_per_epoch=len(train_samples)*AUG_FACTOR, nb_val_samples=len(validation_samples)*AUG_FACTOR,
-                        nb_epoch=5)
-elif MODEL == "nvidia_mod":
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
-    model.add(Convolution2D(64, 3, 3, activation="relu"))
-    model.add(Convolution2D(64, 3, 3, activation="relu"))
-    model.add(Flatten())
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dense(50))
-    model.add(Dense(20))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    history = model.fit_generator(train_generator, validation_data=validation_generator,
-                                  samples_per_epoch=len(train_samples) * AUG_FACTOR,
-                                  nb_val_samples=len(validation_samples) * AUG_FACTOR,
-                                  nb_epoch=8)
-elif MODEL == "simple":
-    model.add(Flatten(input_shape=(160,320,3)))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    history = model.fit_generator(train_generator, validation_data=validation_generator,
-                        samples_per_epoch=len(train_samples)*AUG_FACTOR, nb_val_samples=len(validation_samples)*AUG_FACTOR,
-                        nb_epoch=5)
-elif MODEL == "lenet":
-    model.add(Convolution2D(6,5,5,activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Convolution2D(6,5,5,activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    history = model.fit_generator(train_generator, validation_data=validation_generator,
-                        samples_per_epoch=len(train_samples) * AUG_FACTOR, nb_val_samples=len(validation_samples) * AUG_FACTOR,
-                        nb_epoch=5)
-elif MODEL == "lenet_dropout":
-    model.add(Convolution2D(6,5,5,activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Convolution2D(6,5,5,activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dropout(0.3))
-    model.add(Dense(84))
-    model.add(Dropout(0.3))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    history = model.fit_generator(train_generator, validation_data=validation_generator,
-                        samples_per_epoch=len(train_samples) * AUG_FACTOR, nb_val_samples=len(validation_samples) * AUG_FACTOR,
-                        nb_epoch=5)
-elif MODEL == "nvidia_dropout":
-    model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-    model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-    model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
-    model.add(Convolution2D(64,3,3,activation="relu"))
-    model.add(Convolution2D(64,3,3,activation="relu"))
-    model.add(Flatten())
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dropout(0.3))
-    model.add(Dense(50))
-    model.add(Dropout(0.3))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    history = model.fit_generator(train_generator, validation_data=validation_generator,
-                        samples_per_epoch=len(train_samples)*AUG_FACTOR, nb_val_samples=len(validation_samples)*AUG_FACTOR,
-                        nb_epoch=5)
 
-model.save(H5_FILE.format(MODEL))
+# # Comma.ai model
+# model.add(Convolution2D(16,8,8,subsample=(4,4),border_mode="same",activation="elu"))
+# model.add(Convolution2D(32,5,5,subsample=(2,2),border_mode="same",activation="elu"))
+# model.add(Convolution2D(64,5,5,subsample=(2,2),border_mode="same",activation="elu"))
+# model.add(Flatten())
+# model.add(Dropout(.2))
+# model.add(ELU())
+# model.add(Dense(512))
+# model.add(Dropout(.5))
+# model.add(ELU())
+# model.add(Dense(1))
+# history = model.fit_generator(train_generator, validation_data=validation_generator,
+#                     samples_per_epoch=len(train_samples)*AUG_FACTOR, nb_val_samples=len(validation_samples)*AUG_FACTOR,
+#                     nb_epoch=5)
+
+model.save(H5_FILE.format("0"))
 history_dict = {'loss': history.history['loss'], 'val_loss': history.history['val_loss']}
-pd.DataFrame(data=history_dict).to_csv(CSV_FILE.format(MODEL), index_label='epoch')
+pd.DataFrame(data=history_dict).to_csv(CSV_FILE.format("0"), index_label='epoch')
+
+# del K._SESSION
---
+++ clone_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python clone.py lenet
+python clone.py simple
+python clone.py nvidia
+python clone.py lenet_dropout
+python clone.py nvidia_dropout
